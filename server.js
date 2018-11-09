7ffd7babcaa77d5550963ad7acb869712945921e
cyruslk <cyrus7@hotmail.fr>
2018-11-08 19:04:56 -0500

    wiring the server to the client

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
-
-
+const app = express();
+const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "*");
- res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- next();
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello World!' });
+});
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
 });
 
-
-
-app.listen(port, () => {
-  console.log('listening on port ' + port)
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
